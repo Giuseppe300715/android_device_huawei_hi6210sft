5be6de3679aa8c9fb2213c379ca0060309751196
hak86 <hakamelassouad@gmail.com>
2017-06-20 02:30:52 +0200

    hi6210sft: add Permissions

--- device.mk
+++ device.mk
@@ -28,3 +28,10 @@ PRODUCT_PACKAGES += \
 	libGLES_android \
 	libion
 
+# Permissions
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+        frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+        frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+        frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+        frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
